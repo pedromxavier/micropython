023ecbb1f53da0f24af99b7d32edb3f44491d9cb
Pedro <pedromxavier@poli.ufrj.br>
2019-12-01 09:51:59 -0300

    ...

--- main.py
+++ main.py
@@ -4,10 +4,27 @@ import utime
 led = machine.Pin(2, machine.Pin.OUT)
 
 N = 200_000
-t = utime.time()
-for i in range(N):
-    led.on()
-    led.off()
-T = (utime.time() - t)
+
+@micropython.native
+def piscar():
+    on = led.on
+    off = led.off
+    r = range(N//8)
+    for i in r:
+        on()
+        off()
+        on()
+        off()
+        on()
+        off()
+        on()
+        off()
+        on()
+        off()
+
+t = utime.ticks_ms()
+piscar()
+T = (utime.ticks_ms() - t)
+T = T / 1000
 
 print("Levou {} s, frequencia = {}Hz".format(T, N/T))
